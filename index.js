5177efdd59b9e609e3d251dc1bbdff5fe1b77e4f
rajel89 <rajah.ello@yahoo.com>
2021-08-18 21:36:51 -0500

    included replySchema to comments.js in both models and routes folders.

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Server Start on Port: ${port}`);
 });
+
+
+
+//08.18.2021 - meeting ended with the following needing to be worked on tomorrorw. Need to establish connection to the replySchema
